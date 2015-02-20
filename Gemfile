71f0a5f6ab713b656387825cdf6a38626e8a2e8f
nase00 <nase00@gmail.com>
2015-02-20 05:23:24 -0600

    Update gemfile.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
+# Use Neo4j for database
+gem 'neo4j'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
---
+++ app/models/station.rb
@@ -0,0 +1,11 @@
+class Station
+	include Neo4j::ActiveNode
+
+	property :station_id, type: Integer, constraint: :unique
+	property :name, type: String, constraint: :unique
+	property :latitude, type: Float
+	property :longitude, type: Float
+	property :dpcapacity, type: String
+	property :landmark, type: Integer
+	property :online_date, type: DateTime
+end
