20f58a8a041f30f8e878d1ff0ea5201ad41ed04a
nase00 <nase00@gmail.com>
2015-02-21 15:17:42 -0600

    Add Trip validations.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -16,4 +16,18 @@ class Trip
 	property :user_type, type: String
 	property :gender, type: String
 	property :birthday, type: Integer # Year only
+
+	validates :trip_id,
+						:start_time,
+						:stop_time,
+						:bike_id,
+						:origin_station_id,
+						:destination_station_id,
+						presence: true
+
+	validates :trip_id,
+						:bike_id,
+						:origin_station_id,
+						:destination_station_id,
+						numericality: { only_integer: true }
 end
