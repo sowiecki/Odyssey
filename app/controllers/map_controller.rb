6a53830e6cc8476a6804c660f2e34e279bfca4f3
nase00 <nase00@gmail.com>
2015-02-21 20:39:00 -0600

    Fix file sizes.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,10 @@
 class MapController < ApplicationController
 	def index
+		@trips = Trip.where(bike_id: 361)
+		@hash = Gmaps4rails.build_markers(@trips) do |trip, marker|
+			station = Station.find_by(station_id: trip.origin_station_id)
+		  marker.lat station.latitude
+		  marker.lng station.longitude
+		end
 	end
 end
