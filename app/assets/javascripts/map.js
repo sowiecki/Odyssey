c0b71f1bc11e6d0d00047e7da8426bee4a48c8ee
nase00 <nase00@gmail.com>
2015-02-21 23:51:26 -0600

    Attempt deployment fix

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,55 +1,117 @@
-var mapStyle = [
-  {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}
-];
-var iconBase = 'http://maps.google.com/mapfiles/kml/shapes/';
-var getTrips = $.ajax({
-  url: "markers/361",
-  method: "get",
-  dataType: "json",
-})
+$(function() {
+  var mapStyle = [
+    {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}
+  ];
+  var iconBase = 'http://maps.google.com/mapfiles/kml/shapes/';
+  var getTrips = $.ajax({
+    url: "markers/361",
+    method: "get",
+    dataType: "json",
+  })
 
-getTrips.done(function(trips) {
-  // handler = Gmaps.build('Google');
-  // handler.buildMap({
-  //   provider: {
-  //     styles: mapStyle,
-  //     icon: iconBase + 'schools_maps.png'
-  //   }, internal: {id: 'map'}}, function(){
+  getTrips.done(function(trips) {
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
 
-  //     markers = handler.addMarkers(trips);
-  //     handler.bounds.extendWith(markers);
-  //     handler.fitMapToBounds();
-  //   });
-
-  console.log(trips[1])
-
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  var directionsService = new google.maps.DirectionsService();
-
-  function calcRoute() {
-    var origin      = new google.maps.LatLng(trips[0].lat, trips[0].lng);
-    var destination = new google.maps.LatLng(trips[1].lat, trips[1].lng);
-    var request = {
-        origin:      origin,
-        destination: destination,
-        travelMode:  google.maps.TravelMode.DRIVING
-    };
-    directionsService.route(request, function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        directionsDisplay.setDirections(response);
+    function initialize() {
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+      var mapOptions = {
+        zoom: 6,
+        center: chicago
       }
-    });
-  }
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      directionsDisplay.setMap(map);
+    }
 
-  calcRoute();
+    function calcRoute(trips) {
+      var start = new google.maps.LatLng(trips[1].lat, trips[1].lng);
+      var end = new google.maps.LatLng(trips[4].lat, trips[4].lng);
+      var waypts = [];
+      function pushWaypoints() {
+        waypts.push({
+          location: new google.maps.LatLng(trips[2].lat, trips[2].lng),
+          stopover: true
+        })
+        waypts.push({
+          location: new google.maps.LatLng(trips[3].lat, trips[3].lng),
+          stopover: true
+        })
+      }
 
-  var handler = Gmaps.build('Google');
-  handler.buildMap({
-    provider: {
-      styles: mapStyle,
-      icon: iconBase + 'schools_maps.png'
-    },
-    internal: {id: 'map'}}, function(){
-    directionsDisplay.setMap(handler.getMap());
-  });
+      var request = {
+          origin: start,
+          destination: end,
+          waypoints: waypts,
+          optimizeWaypoints: true,
+          travelMode: google.maps.TravelMode.BICYCLING
+      };
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          console.log(response)
+          directionsDisplay.setDirections(response);
+          // var route = response.routes[0];
+          // var summaryPanel = document.getElementById('directions_panel');
+          // summaryPanel.innerHTML = '';
+          // // For each route, display summary information.
+          // for (var i = 0; i < route.legs.length; i++) {
+          //   var routeSegment = i + 1;
+          //   summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment + '</b><br>';
+          //   summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+          //   summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+          //   summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+          // }
+        }
+      });
+    }
+
+    calcRoute(trips);
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    // handler = Gmaps.build('Google');
+    // handler.buildMap({
+    //   provider: {
+    //     styles: mapStyle,
+    //     icon: iconBase + 'schools_maps.png'
+    //   }, internal: {id: 'map'}}, function(){
+
+    //     markers = handler.addMarkers(trips);
+    //     handler.bounds.extendWith(markers);
+    //     handler.fitMapToBounds();
+    //   });
+
+    // console.log(trips[1])
+
+    // var directionsDisplay = new google.maps.DirectionsRenderer();
+    // var directionsService = new google.maps.DirectionsService();
+
+    // function calcRoute() {
+    //   var origin      = new google.maps.LatLng(trips[0].lat, trips[0].lng);
+    //   var destination = new google.maps.LatLng(trips[1].lat, trips[1].lng);
+    //   var request = {
+    //       origin:      origin,
+    //       destination: destination,
+    //       travelMode:  google.maps.TravelMode.BICYCLING
+    //   };
+    //   directionsService.route(request, function(response, status) {
+    //     if (status == google.maps.DirectionsStatus.OK) {
+    //       directionsDisplay.setDirections(response);
+    //     }
+    //   });
+    // }
+
+    // calcRoute();
+
+    // var handler = Gmaps.build('Google');
+    // handler.buildMap({
+    //   provider: {
+    //     styles: mapStyle,
+    //     icon: iconBase + 'schools_maps.png'
+    //   },
+    //   internal: {id: 'map'}}, function(){
+    //   directionsDisplay.setMap(handler.getMap());
+    // });
+  })
 })
--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,29 @@ require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
+%w(
+  neo4j
+  action_controller
+  action_mailer
+  sprockets
+).each do |framework|
+  begin
+    require "#{framework}/railtie"
+  rescue LoadError
+  end
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Odyssey
   class Application < Rails::Application
+    config.generators do |g|
+      g.orm :neo4j
+    end
+    config.neo4j.session_type = :server_db
+    config.neo4j.session_path = ENV['GRAPHENEDB_URL'] || 'http://localhost:7474'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
