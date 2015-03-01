91825c8efca981ab824bb23bfa66c470a3091fd9
nase00 <nase00@gmail.com>
2015-03-01 09:39:02 -0600

    Test.

--- app/helpers/map_helper.rb
+++ app/helpers/map_helper.rb
@@ -8,10 +8,10 @@ module MapHelper
 	end
 
 	def next_ten_trips
-		Trip.where(bike_id: params[:bike_id].to_i).offset(0).limit(10).order(:start_time).to_a
+		Trip.where(bike_id: params[:bike_id].to_i).order(:start_time).offset(0).limit(10).to_a
 	end
 
 	def next_trip
-		Trip.where(bike_id: params[:bike_id].to_i).offset(10).limit(1).order(:start_time).to_a
+		Trip.where(bike_id: params[:bike_id].to_i).order(:start_time).offset(10).limit(1).to_a
 	end
 end
