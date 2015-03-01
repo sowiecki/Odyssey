7433a5ccfe3b450ea797d8e11434329ddee6a710
nase00 <nase00@gmail.com>
2015-03-01 09:34:45 -0600

    Test.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,20 +5,21 @@ class MapController < ApplicationController
 	end
 
 	def ten_trips
-		hash = Gmaps4rails.build_markers(next_ten_trips) do |trip, marker|
-			station = Station.find_by(station_id: trip.origin_station_id)
-		  marker.lat(station.latitude)
-		  marker.lng(station.longitude)
-		  marker.json({
-		  	trip_id: trip.trip_id,
-		  	# start_time: DateTime.parse(trip.start_time).strftime("%I:%M%p on %m/%d/%Y"),
-		  	# stop_time: DateTime.parse(trip.stop_time).strftime("%I:%M%p on %m/%d/%Y"),
-		  	start_time: trip.start_time,
-		  	stop_time: trip.stop_time,
-		  	duration: trip.trip_duration
-		  })
-		end
-		render json: hash
+		# hash = Gmaps4rails.build_markers(next_ten_trips) do |trip, marker|
+		# 	station = Station.find_by(station_id: trip.origin_station_id)
+		#   marker.lat(station.latitude)
+		#   marker.lng(station.longitude)
+		#   marker.json({
+		#   	trip_id: trip.trip_id,
+		#   	# start_time: DateTime.parse(trip.start_time).strftime("%I:%M%p on %m/%d/%Y"),
+		#   	# stop_time: DateTime.parse(trip.stop_time).strftime("%I:%M%p on %m/%d/%Y"),
+		#   	start_time: trip.start_time,
+		#   	stop_time: trip.stop_time,
+		#   	duration: trip.trip_duration
+		#   })
+		# end
+		array = Trip.where(bike_id: params[:bike_id].to_i).offset(0).limit(10).order(:start_time).to_a
+		render json: array
 	end
 
 	def get_next_trip
