3d661fb85c027646d0833425a768f7be70d12ae2
nase00 <nase00@gmail.com>
2015-03-04 17:01:52 -0600

    Styling changes.

--- app/assets/javascripts/map.js.jsx
+++ app/assets/javascripts/map.js.jsx
@@ -106,7 +106,7 @@ $(function() {
         <div id="map-control-interface">
           <input id="bike-id-input" className="map-control text-field" type="text" autofocus="true" autoComplete="off" placeholder="Enter a bike ID" />
           <input id="start-traverse" className="map-control button-green" onClick={this.startTraverse} type="submit" target="remote" value="Begin" />
-          <p>- or -</p>
+          <p className="click-through">- or -</p>
           <input id="start-traverse" className="map-control button-green" onClick={this.startRandomTraverse} type="submit" target="remote" value="Follow random bike" />
         </div>
       return (
@@ -140,8 +140,8 @@ $(function() {
     },
     render: function() {
       var pause = this.state.started ? 
-          <input id="continue-traverse" className="map-control button-green" onClick={this.continueTraverse} type="submit" target="remote" value="Continue" /> :
-          <input id="pause-traverse" className="map-control button-blue" onClick={this.pauseTraverse} type="submit" target="remote" value="Pause" />
+        <input id="continue-traverse" className="map-control button-green" onClick={this.continueTraverse} type="submit" target="remote" value="Continue" /> :
+        <input id="pause-traverse" className="map-control button-blue" onClick={this.pauseTraverse} type="submit" target="remote" value="Pause" />
       return (
         <div id="map-control-interface">
           <input id="stop-traverse" className="map-control button-red" onClick={this.stopTraverse} type="submit" target="remote" value="Stop" />
@@ -156,15 +156,15 @@ $(function() {
   var RoutesInfoContainer = React.createClass({
     render: function() {
       var key = 0;
-      var routeNodes = this.props.data.reverse().map(function (data) {
+      var routeNodes = this.props.data.map(function (data) {
         return (
             <RouteInfoBox key={key++} data={data} />
           );
         if (key > 10) { key = 10 };
-      });
+      }.bind(this));
       return (
         <div>
-          <ReactCSSTransitionGroup transitionName="routeInfoBox" component="div">
+          <ReactCSSTransitionGroup transitionName="routeInfoBox">
             {routeNodes}
           </ReactCSSTransitionGroup>
         </div>
@@ -203,7 +203,7 @@ $(function() {
       });
       return (
         <div>
-          <ReactCSSTransitionGroup transitionName="routeInfoBox" component="div">
+          <ReactCSSTransitionGroup transitionName="button" transitionAppear={true}>
             {errors}
           </ReactCSSTransitionGroup>
         </div>
