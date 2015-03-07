07e1e9cd8ec73d5cfb5da867fb9ab9452d996ef1
nase00 <nase00@gmail.com>
2015-03-07 10:14:49 -0600

    Flesh out FAQ.

--- app/assets/javascripts/faq.jsx
+++ app/assets/javascripts/faq.jsx
@@ -18,15 +18,29 @@ $(function() {
       var faqPage =
         <div key="faq-popup" id="faq-popup">
           <p id="close"><a href="#" onClick={this.handleClick}>X</a></p>
-          <h2>How does it work?</h2>
+          <h2>How do I use this?</h2>
+          <p>
+            Enter a bike ID (most any number from 1 to 3000) and hit Begin, or just hit "Random Bike."
+          </p>
+          <p>
+            <i>Odyssey</i> will then begin traversing through all of the trips taken by that particular bike during the year 2014. It will start from the first trip of the year and move chronologically forward until it reaches the last trip of the year.
+          </p>
+          <p>
+            Up to 10 trips are kept on the map at a time. Previous trips can be clicked on to snap 
+            the map and streetview back to those coordinates.
+          </p>
+          <h2>How does this work?</h2>
           <p>
             All <a href="https://www.divvybikes.com/datachallenge" target="_blank">Divvy trips made during 2014</a> are 
-            loaded into this application{"'"}s database. Those trips are then cross-referenced with the 
+            loaded in this application{"'"}s database. Those trips are then cross-referenced with the 
             geo coordinates for their origin and destination stations.
           </p>
           <p>
-            If you want to know more about how the maps are created and controlled using those coordinates, 
-            the <a href="https://github.com/Nase00/Odyssey" target="_blank">source code can be viewed here</a>.
+            Routes are approximated for each trip using Google{"'"}s bicycling directions service. 
+          </p>
+          <h2>How can I find the bikes I{"'"}ve ridden?</h2>
+          <p>
+            All public Divvy trip data is anonymized. Also, bike IDs do not appear to be printed anywhere on the bike frames, unfortunately.
           </p>
           <h2>What{"'"}s the tech stack?</h2>
           <ul>
@@ -35,8 +49,15 @@ $(function() {
             <li>
               Frontend: <a href="https://developers.google.com/maps/web/" target="_blank">Google Maps API (of course!)</a> and <a href="http://facebook.github.io/react/" target="_blank">React</a>
             </li>
-            <li>Other: <a href="http://facebook.github.io/react/" target="_blank">Browserify</a> and <a href="http://puma.io/" target="_blank">Puma</a></li>
+            <li>Other: <a href="http://browserify.org/" target="_blank">Browserify</a> and <a href="http://puma.io/" target="_blank">Puma</a></li>
           </ul>
+          <p>
+            If you want to know more about how this application works, 
+            the <a href="https://github.com/Nase00/Odyssey" target="_blank">source code can be viewed here</a>.
+          </p>
+          <p>
+            <a href="http://opensource.com/resources/what-open-source" target="_blank">Learn more about open source here</a>.
+          </p>
         </div>
 
     	if (this.state.clicked) {
