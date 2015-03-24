a50c5e446d0cead22509d7d3c3056c363952494a
nase00 <nase00@gmail.com>
2015-03-23 23:04:17 -0500

    Add basic Jasmine tests.

--- spec/javascripts/route_spec.js
+++ spec/javascripts/route_spec.js
@@ -1,1 +1,12 @@
-//= require components.js
+//= require components
+
+describe('RouteSegments', function() {
+	var routeSegements = new RouteSegments;
+  it("has correct default properties", function() {
+    expect(routeSegements.bikeId).toBe(null);
+    expect(routeSegements.offset).toBe(null);
+    expect(routeSegements.waypts).toBe([]);
+    expect(routeSegements.wayptsInfo).toBe([]);
+    expect(routeSegements.speedInterval).toBe(1400);
+  });
+});
